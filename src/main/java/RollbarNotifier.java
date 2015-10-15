a52934236973e0ebbee48a7db4ee6f7d968aabf5
Mathias Robel <mathias.robel@booxware.de>
2015-10-15 13:40:22 +0200

    first version of appender and empty notifier

---
+++ src/main/java/RollbarNotifier.java
@@ -0,0 +1,31 @@
+import java.net.URL;
+import java.util.Map;
+
+import com.muantech.rollbar.java.NotifyBuilder;
+
+/**
+ * 
+ * @author qunfei, lieharn
+ */
+public class RollbarNotifier {
+
+	private static NotifyBuilder BUILDER;
+	private static URL URL;
+
+	public static void init(String url, String apiKey, String env) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public static void notify(String message, Throwable throwable, Map<String, Object> context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void notify(String message, Map<String, Object> context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
